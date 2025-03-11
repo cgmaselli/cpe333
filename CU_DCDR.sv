85b77fbebbf012f312e69fa97f83fe3e2d65c341
Caroline <129782324+cgmaselli@users.noreply.github.com>
2025-03-10 18:58:33 -0700

    Update CU_DCDR.sv

--- CU_DCDR.sv
+++ CU_DCDR.sv
@@ -7,9 +7,10 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 module CU_DCDR(
-    input logic IR_30,
-    input logic [6:0] IR_OPCODE,
-    input logic [2:0] IR_FUNCT,
+//    input logic IR_30,
+//    input logic [6:0] IR_OPCODE,
+//    input logic [2:0] IR_FUNCT,
+    input logic [31:0] IR,
     // input logic BR_EQ,
     // input logic BR_LT,
     // input logic BR_LTU,
@@ -43,7 +44,8 @@ module CU_DCDR(
         
         //Case statement depending on the opcode for the 
         //instruction, or the last seven bits of each instruction
-        case (IR_OPCODE)
+        // case ir_opcode
+        case (IR[6:0])  // assign instead of splicing?
             7'b0010111: begin // AUIPC
                 ALU_SRCA = 1'b1;
                 ALU_SRCB = 2'b11;
@@ -82,7 +84,8 @@ module CU_DCDR(
                 
                 //Nested case statement
                 //dependent on the function 3 bits
-                case (IR_FUNCT)
+                // case ir_funct
+                case (IR[14:12])
                     3'b000: begin ALU_FUN = 4'b0000; end
                     3'b001: begin ALU_FUN = 4'b0001; end
                     3'b010: begin ALU_FUN = 4'b0010; end
@@ -93,7 +96,7 @@ module CU_DCDR(
                         //dependent on the 30th bit for 
                         //instructions that have the same opcode and 
                         //fucntion 3 bits
-                        case(IR_30)
+                        case(IR[30])
                             1'b0: begin ALU_FUN = 4'b0101; end
                             1'b1: begin ALU_FUN = 4'b1101; end
                             default: begin end
@@ -108,7 +111,8 @@ module CU_DCDR(
                 //ALU_FUN is just the concatenation of
                 //the 30th bit and the function 3 bits
                 RF_WR_SEL = 2'b11;
-                ALU_FUN = {IR_30, IR_FUNCT};
+//                ALU_FUN = {IR_30, IR_FUNCT};
+                ALU_FUN = {IR[30], IR[14:12]};
                 REG_WRITE = 1'b1;
             end
 //            7'b1100011: begin // B-Type
